08bdfb520fdeeb824b31b01aef5e5cb7badec6c4
Anky An <anky25@gmail.com>
2020-03-04 05:06:02 +1000

    Add a tab bar

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'screens/categories_screen.dart';
 import 'screens/category_meals_screen.dart';
 import 'screens/meal_detail_screen.dart';
+import 'screens/tabs_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -29,7 +30,7 @@ class MyApp extends StatelessWidget {
               ),
             ),
       ),
-      home: CategoriesScreen(),
+      home: TabsScreen(),
 //      initialRoute: '/',
       routes: {
         CategoryMealsScreen.routeName: (ctx) => CategoryMealsScreen(),
